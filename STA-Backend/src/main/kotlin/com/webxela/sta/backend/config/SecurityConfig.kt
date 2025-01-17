b5a13005fe9763c2e7a53099f412d2d73eaad7b9
amanverma-765 <akverma4aman@gmail.com>
2025-01-17 12:28:45 +0530

    merge dev branch into prod

--- STA-Backend/src/main/kotlin/com/webxela/sta/backend/config/SecurityConfig.kt
+++ STA-Backend/src/main/kotlin/com/webxela/sta/backend/config/SecurityConfig.kt
@@ -4,13 +4,18 @@ import com.webxela.sta.backend.security.JwtAuthenticationFilter
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
@@ -43,15 +48,25 @@ class SecurityConfig(
             .authorizeExchange { exchanges ->
                 exchanges
                     .pathMatchers(
-                        "/api/v1/sta/auth/**",
+                        "/api/v1/sta/auth/login",
                         "/api/v1/sta/docs",
-                        "/api/v1/sta/**",
-                        "**"
+                        //    "/api/v1/sta/**"
                     ).permitAll()
                     .anyExchange().authenticated()
             }
             .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             .securityContextRepository(securityContextRepository())
+            .exceptionHandling { exceptions ->
+                exceptions.authenticationEntryPoint(NoOpServerAuthenticationEntryPoint())
+            }
             .build()
+    }
+}
+
+class NoOpServerAuthenticationEntryPoint : ServerAuthenticationEntryPoint {
+    override fun commence(exchange: ServerWebExchange, e: AuthenticationException): Mono<Void> {
+        return Mono.fromRunnable {
+            exchange.response.statusCode = org.springframework.http.HttpStatus.UNAUTHORIZED
+        }
     }
 }
